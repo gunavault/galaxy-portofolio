a93ce699106cdebcb4986566c8dc2e33816d8d71
gunavault <gunadharma201@gmail.com>
2026-04-20 19:41:37 +0700

    fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 
-const links = ['About', 'Experience', 'Skills', 'Hobbies', 'Movies', 'Contact','cv'];
+const links = ['About', 'Experience', 'Skills', 'Hobbies', 'Movies', 'Contact'];
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false);
